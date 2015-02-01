5e1f4ee25d8cecbe56a16bd011fec7e1c8cfcac7
wollsau <wolfgang.wedelich@wollistik.de>
2015-02-01 11:00:30 +0100

    added test

---
+++ src/test/java/de/wollistik/dlna/files/FastCheckSumTest.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package de.wollistik.dlna.files;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author Wolfgang Wedelich-John<wolfgang.wedelich@wollistik.de>
+ *
+ */
+public class FastCheckSumTest {
+
+    @Test
+    public void simpleTest() {
+        FastCheckSum chekSum = new FastCheckSum(null);
+    }
+}
